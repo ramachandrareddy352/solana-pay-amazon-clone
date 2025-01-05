b95a0463a859a85755fedf023b1e6f042dfb0e74
tavanam rama <rcrtavanam@gmail.com>
2025-01-05 20:06:41 +0530

    add

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-// require("dotenv").config();
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyBcfkwzoNETXIIKxZ9moMbjjE_9cs3AelA",
+  authDomain: "clone-f50ae.firebaseapp.com",
+  projectId: "clone-f50ae",
+  storageBucket: "clone-f50ae.appspot.com",
+  messagingSenderId: "644834230684",
+  appId: "1:644834230684:web:c96208257b60fa685cc859",
+  measurementId: "G-LZX5ZYX515",
 };
 
 const app = initializeApp(firebaseConfig);
